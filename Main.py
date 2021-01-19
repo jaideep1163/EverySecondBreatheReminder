467e89c44b0f4c67d57580e631a9d74aacdbc19b
jaideep1163 <66325920+jaideep1163@users.noreply.github.com>
2021-01-19 19:37:19 +0530

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,9 @@
+import time
+print("Welcome To My Every Second Breathe Reminder")
+
+def breatheEverySecond():
+    print("Please Breathe")
+
+while True:
+    time.sleep(1)
+    breatheEverySecond()
